ed809dd5f03d77f6f810931f3e1611e145e61ba6
MaNd0o0o0 <saberb45@gmail.com>
2026-06-11 22:43:03 +0300

    Add contact service

--- lib/services/contact_service.dart
+++ lib/services/contact_service.dart
@@ -1,1 +1,19 @@
-[انسخ محتوى contact_service.dart أعلاه]
+// lib/services/contact_service.dart
+import 'package:flutter_contacts/flutter_contacts.dart';
+
+class ContactService {
+  static Future<List<Contact>> getPhoneContacts() async {
+    if (await FlutterContacts.requestPermission()) {
+      final contacts = await FlutterContacts.getContacts(
+        withProperties: true,
+        withPhoto: false,
+      );
+      return contacts;
+    }
+    return [];
+  }
+  
+  static Future<bool> hasPermission() async {
+    return await FlutterContacts.requestPermission();
+  }
+}
